dcf4c257774ca4ce8564b46e731df1779b2a2b67
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 18:49:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,8 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name:
-        age:
+        name: "Lord montague", age: "53"
         },
       :matriarch => {
         
