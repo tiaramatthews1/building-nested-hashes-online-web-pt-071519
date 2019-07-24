74dae8e9253a90a00b1caf468d272b16644aa927
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 18:50:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,10 +5,10 @@ def third_challenge
         name: "Lord montague", age: "53"
         },
       :matriarch => {
-        
+        name: "Lady montague", age: "54"
         },
       :hero => {
-        
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
