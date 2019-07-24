3c0daa5b829a82e5ff6eb374cfbe22c453386931
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 19:05:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,2 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :partiarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => {}
-      }
-    }
-
-  end
+  
